3b297661098d28241bb8b3f9fd210d1bfc05b66e
afoninaanna <afoninaa903@gmail.com>
2023-08-22 01:19:12 +0400

    add: component posts

--- src/app/components/posts/posts.component.ts
+++ src/app/components/posts/posts.component.ts
@@ -1,10 +1,13 @@
-import { Component } from "@angular/core";
+import { IPost } from './../../models/posts';
+import { Component, Input } from "@angular/core";
 
 @Component({
     selector: 'posts',
-    templateUrl: './posts.component.html'
+    templateUrl: './posts.component.html',
+    styleUrls: ['./posts.component.css']
 })
 
 export class PostsComponent {
-    
+    @Input() posts: IPost[];
+    displayedColumns: string[] = ['userId', 'id', 'title', 'body'];
 }
--- src/app/modules/material.module.ts
+++ src/app/modules/material.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
+import { MatTableModule } from '@angular/material/table';
 
 
 @NgModule({
     imports: [
         MatCardModule,
-        MatButtonModule
+        MatButtonModule,
+        MatTableModule
     ],
     exports: [
         MatCardModule,
-        MatButtonModule
+        MatButtonModule,
+        MatTableModule
     ]
 })
 export class MaterialModule { }
